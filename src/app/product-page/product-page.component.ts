51c4add0ff4a9eee8091f00c0cb987b585e23d45
yunxi <yunxi@activekm.com>
2026-04-29 18:44:07 +0800

    feat(product-page): 新增產品功能實作

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -24,6 +24,20 @@ export class ProductPageComponent {
 
   protected readonly products = signal<Product[]>([]);
 
+  protected onAdd(): void {
+    const product = new Product({
+      name: '書籍 Z',
+      authors: ['作者甲', '作者乙', '作者丙'],
+      company: '博碩文化',
+      isShow: true,
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      createDate: new Date('2025/4/9'),
+      price: 10000,
+    });
+    this.productService.add(product);
+    this.getProducts(this.pageIndex(), this.pageSize());
+  }
+
   constructor() {
     effect(() => {
       const pageIndex = this.pageIndex();
