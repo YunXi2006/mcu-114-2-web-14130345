6c802f82f3d9c60008eae6a218b7968c77c08bf9
yunxi <yunxi@activekm.com>
2026-05-20 17:04:23 +0800

    feat(product-form): 產品表單更新作業實作

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -141,4 +141,7 @@ export class ProductService {
       filter(({ id }) => id === productId)
     );
   }
+  update(product: Readonly<Product>): Observable<Product> {
+    throw new Error('Not implement');
+  }
 }
