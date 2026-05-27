add5b610c056d2df33a522cb87622ec207a21bfc
yunxi <yunxi@activekm.com>
2026-05-27 16:59:07 +0800

    feat(product): 實作從 azure 服務取得產品資料

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { ProductAzureService } from './services/product-azure.service';
 import { ProductService } from './services/product.service';
-import { ProductRemoteService } from './services/product-remote.service';
-import { provideHttpClient } from '@angular/common/http';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideHttpClient(),
     provideRouter(routes, withComponentInputBinding()),
-    { provide: ProductService, useClass: ProductRemoteService },
+    { provide: ProductService, useClass: ProductAzureService },
   ],
 };
--- src/app/services/product-azure.service.spec.ts
+++ src/app/services/product-azure.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ProductService } from './product.service';
+import { ProductAzureService } from './product-azure.service';
 
-describe('ProductService', () => {
-  let service: ProductService;
+describe('ProductAzureService', () => {
+  let service: ProductAzureService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(ProductService);
+    service = TestBed.inject(ProductAzureService);
   });
 
   it('should be created', () => {
